1b92c06e1d56c1837542859e1a1439381f086ff2
amir <amir.sediqi69@gmail.com>
2021-08-26 20:26:41 +0200

    Initial Commit

---
+++ Quotes/QuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotesApp.swift
+//  Quotes
+//
+//  Created by Amir Sediqi on 2021-08-26.
+//
+
+import SwiftUI
+
+@main
+struct QuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
